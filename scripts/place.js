35d02cfcb642a6c378da78439cb40717bca685a7
Owen-kasule <owenatug@gmail.com>
2024-11-11 20:04:12 +0300

    Enhance header and footer functionality in place.html and place.js, and improve card overlay styles in place.css for better readability and layout consistency

--- scripts/place.js
+++ scripts/place.js
@@ -38,3 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+// Update copyright year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Update last modified date
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
